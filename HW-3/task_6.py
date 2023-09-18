4f595367098e067f13c6c467e8337b2d91241aa3
Eugene <eugene@platform500.com>
2023-09-19 00:02:25 +0300

    more readable code

--- HW-3/task_6.py
+++ HW-3/task_6.py
@@ -5,7 +5,7 @@ money = float(input())
 ua_usd, usd_ua = 37.99, 37.39
 ua_eur, eur_ua = 41.10, 40.10
 
-print(f'{money:.2f}', 'UAH --> USD', f'{money / ua_usd:.2f}',
-      '\n'f'{money:.2f}', 'USD --> UAH', f'{money * usd_ua:.2f}',
-      '\n'f'{money:.2f}', 'UAH --> EUR', f'{money / ua_eur:.2f}',
-      '\n'f'{money:.2f}', 'EUR --> UAH', f'{money * eur_ua:.2f}')
+print(f'{money:.2f}', 'UAH --> USD', f'{money / ua_usd:.2f}', '\n'
+      f'{money:.2f}', 'USD --> UAH', f'{money * usd_ua:.2f}', '\n'
+      f'{money:.2f}', 'UAH --> EUR', f'{money / ua_eur:.2f}', '\n'
+      f'{money:.2f}', 'EUR --> UAH', f'{money * eur_ua:.2f}')
