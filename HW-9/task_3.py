fccc581e3a4cdd574659b3e5bddecae144b2b4c0
yevhenii spivak <yevhenii.spivak@gmail.com>
2023-10-19 10:08:21 +0300

    rename Error, task_3

--- HW-9/task_3.py
+++ HW-9/task_3.py
@@ -5,4 +5,4 @@ course = {'title': 'AQA', 'language': 'Python', 'level': 'PRO', 'frame': 'pytest
 try:
     print(f'Value: {course[key]}')
 except KeyError:
-    print('Key error')
+    print('Key not found')
