fdf800d8a9c8973aeabbb0dba778ffc2bcafa672
yevhenii spivak <yevhenii.spivak@gmail.com>
2023-10-19 16:30:13 +0300

    added check for numbers, task_2

--- HW-10/task_2.py
+++ HW-10/task_2.py
@@ -9,7 +9,8 @@ def is_prime(number):
         x += 2
     return x * x > number
 
-if 2 < number < 1001:
+
+if 2 <= number <= 1001:
     print(f'{number} is prime: {is_prime(number)}')
 else:
     print(f'Please enter a number between 2 and 1000, your nubmer {number} isn\'t suitable')
