5fa681e3a65788497f90d41317f615e01aeee575
Yevhenii Spivak <63404282+YevheniiSpivak@users.noreply.github.com>
2023-11-13 17:14:52 +0200

    added HW-10 (#12)
    * added HW-10
    
    * added check for numbers, task_2
    
    * added check for numbers, task_2
    
    * refactoring, task_2

---
+++ HW-10/task_2.py
@@ -0,0 +1,16 @@
+number = int(input('Enter a number from 2 to 1000: '))
+
+
+def is_prime(number):
+    if number % 2 == 0:
+        return number == 2
+    x = 3
+    while x * x <= number and number % x != 0:
+        x += 2
+    return x * x > number
+
+
+if 2 <= number <= 1000:
+    print(f'{number} is prime: {is_prime(number)}')
+else:
+    print(f'Please enter a number between 2 and 1000, your nubmer {number} isn\'t suitable')
